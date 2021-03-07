471e40d0be0ff6c476b56f6b7197282104e83f9d
DonQueso89 <kg.v.ekeren@gmail.com>
2021-03-07 12:44:44 +0100

    revamp about page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,32 +2,45 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 
-const Contact = ({ data }) => (
-  <Layout pageTitle="Contact">
-    <SEO title="Contact" />
-    <div
-      className="contact-container"
-    ></div>
-    <h1 className="page-title">About</h1>
-    <div
-      className="contact-content"
-      dangerouslySetInnerHTML={{
-        __html: data.allDatoCmsContact.edges[0].node.bodyNode.childMarkdownRemark.html,
-      }}
-    ></div>
-  </Layout>
-)
+const Contact = ({ data }) => {
+  const payload = data.allDatoCmsContact.edges[0].node
+  return (
+    <Layout pageTitle="Contact" info={payload.infoNode}>
+      <SEO title="Contact" />
+      <h1 className="page-title">About</h1>
+      <div className="contact-container">
+          <Img fixed={payload.image.fixed} className="contact-img" imgStyle={{objectFit: "contain", objectPosition: "left top"}}/>
+        <div
+          className="contact-content"
+          dangerouslySetInnerHTML={{
+            __html: payload.bodyNode.childMarkdownRemark.html,
+          }}
+        ></div>
+      </div>
+    </Layout>
+  )
+}
 
 export default Contact
-
 
 export const query = graphql`
   query c {
     allDatoCmsContact {
       edges {
         node {
+          image {
+            fixed {
+              ...GatsbyDatoCmsFixed
+            }
+          }
           bodyNode {
+            childMarkdownRemark {
+              html
+            }
+          }
+          infoNode {
             childMarkdownRemark {
               html
             }
@@ -37,4 +50,3 @@ export const query = graphql`
     }
   }
 `
-
